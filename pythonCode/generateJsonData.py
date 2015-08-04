61a70a32cb265000bd330e622603c151cf813e32
ycheng1947 <ycheng1947@gmail.com>
2015-08-04 23:48:27 +0800

    add a python code of generating random data

---
+++ pythonCode/generateJsonData.py
@@ -0,0 +1,11 @@
+import random 
+def  generate():
+	fp = open("D:\\code\\Record-of-Learning\\pythonCode\\test.txt","w")
+	for i in range(0,1000):
+		a = random.randint(1,10)
+		fp.write(str(a)+"\n")
+
+	fp.close()
+
+if __name__ == '__main__':
+	generate()
